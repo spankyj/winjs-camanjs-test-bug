2fb1ce3de8fa56999ef1fec6bb1e2b242d6bbb14
Lee Mallon <lee@rarelyimpossible.com>
2014-06-27 10:39:02 +0100

    test files

---
+++ Web/app.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function() {
+    $("a").click(function (ev) {
+        console.log('clicked');
+        Caman("#image", function () {
+            this.brightness(10);
+            this.contrast(30);
+            this.sepia(60);
+            this.saturation(-30);
+            this.render();
+            this.render(function () {
+
+            });
+        });
+        ev.preventDefault();
+    });
+});
